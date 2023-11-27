db77ef487b63a1b77c0bd9a639b5ed20381a1f66
anikdas400 <anikchandra400@gmail.com>
2023-11-27 11:09:13 +0600

    error page complite

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -5,6 +5,10 @@ import { FcGoogle } from "react-icons/fc";
 const Login = () => {
     const handleLogin=event=>{
         event.preventDefault()
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email,password)
     }
     return (
         <div className="hero min-h-screen bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% rounded-t-lg">
@@ -12,7 +16,7 @@ const Login = () => {
             <div className="hero-content flex-col lg:flex-row">
                 <div className="w-1/2 mr-12">
 
-                    <img className="h-[30rem] rounded-lg" src="https://i.ibb.co/TksptWD/login.jpg" alt="" />
+                    <img className="h-[30rem] rounded-lg" src="https://i.ibb.co/3hv0KQd/login.jpg" alt="" />
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <h1 className="text-5xl font-semibold text-center text-indigo-500 mt-5">Login now!</h1>
@@ -41,9 +45,9 @@ const Login = () => {
                     } */}
 
                     <p className="text-center mb-3">Do not have an account?please <Link className="text-purple-700 font-bold text-lg" to="/signup">Sign Up</Link> </p>
-                    <h2 className="text-center text-lg font-semibold text-purple-900 underline">OR</h2>
+                    <div className="divider divider-accent w-8/12 mx-auto text-purple-700 italic font-bold">OR</div>
 
-                    <p className="text-center">sign in with<button className="btn btn-ghost font-bold text-purple-900 text-lg"><FcGoogle className="text-lg" />Google</button></p>
+                    <p className="text-center mb-5">sign in with<button className="btn btn-ghost font-bold text-purple-900 text-lg"><FcGoogle className="text-lg" />Google</button></p>
 
 
 
