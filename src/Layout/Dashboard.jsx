6077c080d6511815f20ad0876be4f62b44a7b1c5
anikdas400 <anikchandra400@gmail.com>
2023-11-28 16:10:53 +0600

    user dashboard with wish list

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { FaShoppingCart } from "react-icons/fa";
+import { FaBookmark, FaCalendar, FaHome, FaRev, FaShoppingCart } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 import useCart from "../hook/useCart";
 
@@ -11,16 +11,34 @@ const Dashboard = () => {
             {/* dashboard side bar */}
             <div className="w-64 min-h-screen bg-emerald-300 p-4">
                 <div className="ml-6">
-                    <h2 className="text-xl font-bold">JCX</h2>
+                    <h2 className="text-xl font-bold text-yellow-300">JCX</h2>
                     <h3 className="text-lg font-semibold">Real Estate Industries</h3>
                 </div>
                 <ul className="menu">
-                <li>
-                        <NavLink to='/dashboard/cart'><FaShoppingCart></FaShoppingCart> My Cart ({cart.length})</NavLink>
+                    <li>
+                        <NavLink to='/dashboard/userHome'><FaHome></FaHome> User Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/dashboard/cart'><FaShoppingCart></FaShoppingCart>My Wishlist ({cart.length})</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/dashboard/reservation'><FaCalendar></FaCalendar> Reservation</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/dashboard/review'><FaRev></FaRev>Add a Review</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/dashboard/bookings'><FaBookmark></FaBookmark> My Bookings</NavLink>
+                    </li>
+
+                    <div className="divider divider-info">OR</div>
+
+                    <li>
+                        <NavLink to='/dashboard/userHome'><FaHome></FaHome> User Home</NavLink>
                     </li>
 
                 </ul>
-                
+
             </div>
 
             {/* dashboard content */}
--- src/Pages/Dashboard/Cart/Cart.jsx
+++ src/Pages/Dashboard/Cart/Cart.jsx
@@ -1,9 +1,103 @@
+import useAxiosSecure from "../../../hook/useAxiosSecure";
+import { FaTrashAlt } from "react-icons/fa";
+import useCart from "../../../hook/useCart";
+import Swal from "sweetalert2";
 
 
 const Cart = () => {
+    const [cart, refatch] = useCart()
+    const totalPrice = cart.reduce((total, item) => total + item.price, 0)
+    const axiosSecure = useAxiosSecure()
+
+    const handleDelete = id =>{
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                axiosSecure.delete(`/wishs/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            refatch()
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your Wish Property has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    }
+                    )
+            }
+        });
+
+
+    }
     return (
         <div>
-            <h3>my cart</h3>
+           <div className="flex justify-evenly">
+                <h2 className="text-3xl font-semibold">Total orders: {cart.length}</h2>
+                <h3 className="text-3xl font-semibold">Total price: ${totalPrice}</h3>
+                <button className="btn btn-secondary text-2xl font-semibold">pay</button>
+            </div>
+
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr className="text-xl text-black font-bold bg-orange-300">
+                            <th>
+                                #
+                            </th>
+                            <th>ITEM IMAGE</th>
+                            <th>ITEM NAME</th>
+                            <th>PRICE</th>
+                            <th>ACTION</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            cart.map((item, index) => <tr key={item._id}>
+                                <th>
+                                    {index + 1}
+                                </th>
+                                <td>
+                                    <div className="flex items-center gap-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src={item.image} alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </td>
+                                <td className="text-lg">
+                                    {item.name}
+
+                                </td>
+                                <td className="text-lg">${item.price}</td>
+                                <th>
+                                    <button
+                                        onClick={() => handleDelete(item._id)}
+                                        className="btn btn-ghost btn-lg"><FaTrashAlt className="text-red-600"></FaTrashAlt>
+                                    </button>
+                                </th>
+                            </tr>)
+                        }
+
+
+
+                    </tbody>
+
+
+                </table>
+            </div>
         </div>
     );
 };
